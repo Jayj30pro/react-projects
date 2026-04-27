f303b966243ba8bd3bfce85d492aa45ea511f992
Jason <jayj3.0pro@gmail.com>
2026-04-26 20:52:14 -0600

    age-guesser

--- event-handeling/src/index.js
+++ event-handeling/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Agerater() {
+  const [year, setYear] = useState(2026);
+
+  function handleChange(e) {
+    setYear(e.target.value);
+  }
+
+  function getAge(year) {
+    return (2026 - year);
+  }
+
+  return <div>
+    <input type='text' value={year} onChange={handleChange} />
+    <p> Since you were born in {year} you are {getAge(year)} years old </p>
+  </div>
+
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Agerater />
 );
 
 // If you want to start measuring performance in your app, pass a function
