d3b6e63d565d30ea035636b7f477b26c61daa683
Jason <jayj3.0pro@gmail.com>
2026-04-26 22:14:22 -0600

    event-math-forms

--- event-handeling/src/index.js
+++ event-handeling/src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function Agerater() {
+function MathForm() {
   const [year, setYear] = useState(2026);
+  const [product, setProduct] = useState(1);
+  const [num, setNum] = useState(0);
+
 
   function handleChange(e) {
     setYear(e.target.value);
@@ -15,16 +18,35 @@ function Agerater() {
     return (2026 - year);
   }
 
+  function handleMath(e) {
+    setNum(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    if (Number(num) != 0){
+      setProduct(product * Number(num));
+      e.preventDefault();
+    }
+    else {
+      alert("no multiplying by zero");
+    }
+    
+  }
+
   return <div>
     <input type='text' value={year} onChange={handleChange} />
     <p> Since you were born in {year} you are {getAge(year)} years old </p>
+    <input type='number' value={num} onChange={handleMath} />
+    <button onClick={handleSubmit}>Multiply</button>
+    <p> The product is {product} </p>
+    <p> The number in the system is {num} </p>
   </div>
 
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Agerater />
+  <MathForm />
 );
 
 // If you want to start measuring performance in your app, pass a function
