1a263da6394ff929d7ea9021ec8ff2647cb8e6f5
Jason <jayj3.0pro@gmail.com>
2026-05-14 22:35:14 -0600

    home-page-style-start

--- routing-test/src/pages/Home.js
+++ routing-test/src/pages/Home.js
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h2>Home Page</h2>;
+  return (
+  <div className="home-page">
+    <h2>Home Page</h2>
+    <div className="home-message">
+      <p>Welcome to ColdFlare Studios</p>
+    </div>
+  </div>
+    );
 }
